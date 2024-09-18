7184de2bfff996dc787d114483c561fa14da2686
Cecilie Zhang <zhan4650@eceprog4.ecn.purdue.edu>
2024-09-18 11:19:26 -0400

    Implemented function that determines whether or not the user wants to open, close or switch

--- a4.c
+++ a4.c
@@ -2,22 +2,38 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+
 #define INPUTMAX 100
 
 void inputFun(char *);
 long int getNumb(char *);
+int detMode(char *);
 
 int main(void) {
   //int * windowArray = calloc(10, sizeof(int));
 
   do {
-    char inputArray [INPUTMAX];
+    char inputArray[INPUTMAX];
     inputFun(inputArray); //Get input
+    int mode = detMode(inputArray); //Gets the mode (open -> 1, close -> 2, switch -> 3)
     long int numb = getNumb(inputArray); //Extrating the number from the input
+
     
+
+    printf("Number: %ld\n", numb);
+    printf("Mode: %d\n", mode);
   }while(1);
 
   return 0;
+}
+
+int detMode(char * inputArray) {
+  switch (inputArray[0]) {
+    case 'o' : return 1; break;
+    case 'c' : return 2; break;
+    case 's' : return 3; break;
+    default: return 0;
+  }
 }
 
 long int getNumb(char * inputArray) {
@@ -39,6 +55,6 @@ long int getNumb(char * inputArray) {
 }
 
 void inputFun(char * inputArray) {
-    scanf("\n%[^\n]%*c", inputArray);
+    scanf("%[^\n]%*c", inputArray);
     return;
 }
